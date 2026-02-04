c6e7ad6aa71860060af3814298b9b8193775e499
dsyhub <77458971+dsyhub@users.noreply.github.com>
2026-02-03 19:10:03 -0800

    removed test sql queries

--- sql/order_items.sql
+++
@@ -1,3 +0,0 @@
-SELECT *
-FROM `bigquery-public-data.thelook_ecommerce.order_items`
-WHERE DATE(created_at) < @cutoff_date
--- sql/orders.sql
+++
@@ -1,5 +0,0 @@
--- SELECT DISTINCT user_id
-SELECT *
-FROM `bigquery-public-data.thelook_ecommerce.orders`
-WHERE DATE(created_at) BETWEEN @cutoff_date
-  AND DATE_ADD(@cutoff_date, INTERVAL @prediction_window_days DAY)
